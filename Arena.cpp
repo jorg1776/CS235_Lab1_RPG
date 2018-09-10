114c84d17c75ef431be38b31efd5726c7799dd80
Jordan Gruenewald <jorg1776@byu.edu>
2018-09-10 17:08:00 -0600

    Inherited from Interface

--- Arena.cpp
+++ Arena.cpp
@@ -10,6 +10,26 @@ Arena::~Arena()
 {
 }
 
+bool Arena::addFighter(std::string info)
+{
+	return false;
+}
+
+bool Arena::removeFighter(std::string name)
+{
+	return false;
+}
+
+FighterInterface * Arena::getFighter(std::string name)
+{
+	return nullptr;
+}
+
+int Arena::getSize() const
+{
+	return 0;
+}
+
 
 Arena::Arena()
 {
--- Arena.h
+++ Arena.h
@@ -1,11 +1,21 @@
 #ifndef ARENA_HPP_
 #define ARENA_HPP_
 
-class Arena
+#include "ArenaInterface.h"
+
+class Arena : ArenaInterface
 {
+private:
+
 public:
 	Arena();
 	~Arena();
+
+	// Inherited via ArenaInterface
+	virtual bool addFighter(std::string info) override;
+	virtual bool removeFighter(std::string name) override;
+	virtual FighterInterface * getFighter(std::string name) override;
+	virtual int getSize() const override;
 };
 
 #endif /* ARENA_HPP_*/
